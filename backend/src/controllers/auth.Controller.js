ee54ec31014c1f9e2fd1b806a1a893b1dbd3d0b9
Dev Jeff <arribion@gmail.com>
2025-10-07 04:17:14 +0300

    connect to database and set someboilerplates

--- backend/src/controllers/auth.Controller.js
+++ backend/src/controllers/auth.Controller.js
@@ -2,11 +2,18 @@
 // username
 // userEmail
 // userPassword
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import { json } from "body-parser";
 import authmodel from "../models/auth.Model.js";
 
 export const registerUser = async (req, res) => {
+  /* This code snippet is extracting the `username`, `email`, and `password` properties from the
+  `req.body` object using object destructuring. It then checks if any of these values are falsy
+  (empty, null, or undefined). If any of these fields are missing, it returns a JSON response
+  indicating that all fields are required. This is a basic validation step to ensure that the
+  required user registration fields are provided before proceeding with the registration process. */
     const { username, email, password } = req.body;
     if (!username || !email || !password) {
         return res.json({
@@ -32,8 +39,10 @@ export const registerUser = async (req, res) => {
         password: hashedPassword
     }
 
-    const User = authmodel.newUser()
-    User.save()
+    const User = authmodel(newUser);
+    await User.save();
+
+    // generating a jsonwebtoken
 
 }
 
