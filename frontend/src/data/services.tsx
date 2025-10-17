5dccb8f1e22346a88bef2e4cb3abe63e8f9b3311
Dev Jeff <arribion@gmail.com>
2025-10-17 11:35:29 +0300

    set services

---
+++ frontend/src/data/services.tsx
@@ -0,0 +1,68 @@
+import { MdOutlineContentPasteGo } from "react-icons/md";
+import { IoIosPeople } from "react-icons/io";
+import { FaRegNewspaper } from "react-icons/fa6";
+import { MdOutlineSendTimeExtension } from "react-icons/md";
+import { BsCash } from "react-icons/bs";
+import { FaPhotoVideo } from "react-icons/fa";
+import { LiaDigitalTachographSolid } from "react-icons/lia";
+
+type blogserivice = {
+  id: number;
+  title: string;
+  description: string;
+  icon: JSX.Element;
+};
+
+export const services: blogserivice[] = [
+  {
+    id: 1,
+    title: "Content Publishing",
+    description:
+      "We deliver regular blog posts on lifestyle, tech, travel, food, and personal development to keep you informed and inspired.",
+    icon: <MdOutlineContentPasteGo/>,
+  },
+  {
+    id: 2,
+    title: "Commenting & Community",
+    description:
+      "Join the conversation—share your thoughts, connect with others, and be part of a growing community of curious minds.",
+    icon: <IoIosPeople/>,
+  },
+  {
+    id: 3,
+    title: "Newsletter Subscription",
+    description:
+      "Subscribe to receive curated updates, exclusive content, and the latest blog highlights straight to your inbox.",
+    icon: <FaRegNewspaper/>,
+  },
+  {
+    id: 4,
+    title: "Affiliate Marketing",
+    description:
+      "Explore recommended products and services through affiliate links that help support our blog while offering you value.",
+    icon: <MdOutlineSendTimeExtension/>,
+  },
+  {
+    id: 5,
+    title: "Sponsored Posts",
+    description:
+      "We collaborate with brands to bring you sponsored content that aligns with your interests and our values.",
+    icon: <BsCash/>,
+  },
+  {
+    id: 6,
+    title: "Ad Placement",
+    description:
+      "Our blog features relevant ads (Google AdSense, banners) to generate revenue while keeping your experience smooth.",
+    icon: <FaPhotoVideo/>,
+  },
+  {
+    id: 7,
+    title: "Digital Products",
+    description:
+      "Access downloadable resources like eBooks, templates, and online courses crafted to help you grow and learn.",
+    icon: <LiaDigitalTachographSolid/>,
+  },
+];
+
+export default services;
--- frontend/src/pages/Services.tsx
+++ frontend/src/pages/Services.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import { FaArrowRight } from "react-icons/fa";
+
+import services from "../data/services";
 
 const Services = () => {
   return (
-    <section>
-      <h1>Services Page</h1>
-    </section>
-  )
-}
+    <>
+      <section className="service-hero">
+        <div>
+          <h1 className="font-bold font-5xl text-blue-800">
+            Empowering Your Journey Through <br /> Content, Creativity, and
+            Connection.
+          </h1>
+          <p>
+            Welcome to the heart of what we offer—where ideas meet impact.
+            Whether you're here to explore insightful articles, discover helpful
+            resources, or engage with a vibrant community, our services are
+            designed to elevate your experience. From personalized content and
+            expert advice to interactive tools and exclusive member perks, we’re
+            here to support your goals and spark your curiosity.
+          </p>
+        </div>
+      </section>
+      <section className="services-s-conttainer">
+        <h1>📝 Core Services</h1>
+        <div className="services-container">
+          {
+            services.map((service) => (
+              <div key={service.id} className="service-card">
+                <div>
+                  {service.icon}
+                </div>
+                <div>
+                  <h2>{service.title}</h2>
+                  <p> {service.description} </p>
+                  <p>
+                    learn more <FaArrowRight />{" "}
+                  </p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Services
+export default Services;
